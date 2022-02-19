e12a3acd820d1b03ba45ac7f3e6f2d516b571108
YoctoBit <42770197+YoctoBit@users.noreply.github.com>
2022-02-19 15:19:17 +0300

    После проверки ESLint

--- js/main.js
+++ js/main.js
@@ -1,22 +1,13 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
 const getRandomInt = function(min, max) {
-	return Math.abs(Math.floor(Math.random() * (max - min)) + min);
-}
-console.log (getRandomInt(-295.23, "-290.23"));
+  return Math.abs(Math.floor(Math.random() * (max - min)) + min);
+};
+console.log (getRandomInt(-295.23, '-290.23'));
 
 
-// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. 
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 const getRandomFloat = function(min, max, accuracy) {
-	let rand = Math.random() * (max - min) + min;
-	return Math.abs(rand.toFixed (accuracy > 16 || accuracy < -16 ? 16 : Math.abs(accuracy)));
-}
+  const rand = Math.random() * (max - min) + min;
+  return Math.abs(rand.toFixed (accuracy > 16 || accuracy < -16 ? 16 : Math.abs(accuracy)));
+};
 console.log (getRandomFloat(0.638, -0.3434321, -1454));
-
-
-// Обe функции переваривают любые числа (в том числе и строки), вне зависимости какое из них больше и выдают:
-// getRandomFloat рандомное положительное целое число,
-// getRandomFloat вещественное число.
-// getRandomFloat также защищен от получения отрицательного количества знаков после запятой "accuracy"
-// и не показывает количество превышающее 16.
-// возможно в будущем есть смысл переделать getRandomFloat с использованием метода toPrecision, чтобы ограничить точность всего числа
-// и избежать отображения "мусора"
